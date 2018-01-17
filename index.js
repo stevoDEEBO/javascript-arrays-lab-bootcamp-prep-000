a9835d07d67e6278291566bc2f69cb60a25794bb
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-17 06:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  [name, ...kittens]
+}
+
+function removeLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function removeFirstKitten() {
+  kittens.shift()
+  return kittens
+}
